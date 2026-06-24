701b29f84fed9040e62eae6e468f5de38c2f6049
Maximilian Michel <maxi.michel@gmx.net>
2026-06-24 13:19:35 +0200

    Remove unused constants in frontend

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -1,19 +1,4 @@
 /**
- * Default character snippet length to be displayed
- */
-export const PAGINATION_SIZE: number = 1000;
-
-/**
- * Delay in miliseconds before a new entry is pushed to the edit history stack (reduces overhead on rapid typing)
- */
-export const EDIT_DELAY: number = 1000;
-
-/**
  * Delay in miliseconds before fetching new data based on changed parameters
  */
 export const FETCH_DELAY: number = 300;
-
-/**
- * Max number of edit history entries. After that, the oldest ones will be deleted
- */
-export const HISTORY_MAX_SIZE: number = 25;
